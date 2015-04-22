2655666eb70f2259ca732d1d92e8f7d3d5b55c62
Gunawan <gunawan.my.id@gmail.com>
2015-04-22 22:07:27 +0800

    Update laporan1.java
    lumayan, tapi di revisi dulu yah..^_^

--- laporan1.java
+++ laporan1.java
@@ -1,50 +1,50 @@
 package prak1nita;
 
-import javax.xml.crypto.Data;
+import javax.xml.crypto.Data; // INI MAKSUDNYA APAAN YAH? ..=), SILAHKAN BUAT TANPA IMPORT DECLARATION DULU ..=)
 
-public class laporansatu {
+public class laporansatu { //BERIKAN PENJELASAN
 	public static void main(String args[]) {
 		data();
-		jumlah();  //pemanggilan method
+		jumlah();  //pemanggilan method METHOD APA? ..=)
 		kurang();
 		kali();
 		bagi();
 	}
 	
-	static void data() {
+	static void data() { //BERIKAN PENJELASAN
 		String nama="nureksanita"; //mendeklarasikan varaibel lokal
 		String alamat="samata";//mendeklarasikan varaibel lokal
 		byte umur=20;//mendeklarasikan varaibel lokal
-		System.out.println("Saya bernama "+nama);
+		System.out.println("Saya bernama "+nama); //BERIKAN PENJELASAN
 		System.out.println("Dengan alamat di "+alamat);
 		System.out.println("Sudah tua dengan umur "+umur);
 	}
-	static void jumlah() {
+	static void jumlah() { //BERIKAN PENJELASAN
 		int a,b,c; //mendeklarasikan varaibel lokal
-		a=10;
-		b=10;
-		c=a+b;
+		a=10; //SEDEHANAKAN
+		b=10;  //SEDEHANAKAN
+		c=a+b; //BERIKAN PENJELASAN
 		System.out.println("hasil penjumlahan "+a+" dan "+b+" adalah= "+c);
 	}
-	static void kurang() {
+	static void kurang() { //BERIKAN PENJELASAN
 		int a,b,c; //mendeklarasikan varaibel lokal
 		a=10;
 		b=10;
 		c=a-b;
 		System.out.println("hasil pengurangan "+a+" dan "+b+" adalah= "+c);
 	}
-	static void kali() {
+	static void kali() { //BERIKAN PENJELASAN
 		int a,b,c; //mendeklarasikan varaibel lokal
-		a=2;
-		b=10;
-		c=a*b;
+		a=2; //-SDA -
+		b=10; //-SDA -
+		c=a*b; //-SDA -
 		System.out.println("hasil perkalian "+a+" dan "+b+" adalah= "+c);
 	}
-	static void bagi() {
+	static void bagi() { //BERIKAN PENJELASAN
 		float a,b,c; //mendeklarasikan varaibel lokal
-		a=40;
-		b=3;
-		c=a/b;
+		a=40; //-SDA -
+		b=3; //-SDA -
+		c=a/b; //-SDA -
 		System.out.println("hasil pembagian "+a+" dan "+b+" adalah= "+c);
 	}
 }
